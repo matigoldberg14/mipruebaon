8ee43f0c113b1265a37b049b34eb7492fdf40eb5
matigoldberg14 <98336460+matigoldberg14@users.noreply.github.com>
2023-02-22 16:39:33 -0300

    Updates

---
+++ precache-manifest.220f53f207e555cbf1ee09c99c2250ef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "eca4012f3f90d0d01d77",
+    "url": "/mipruebaon/static/css/main.4d107cb2.chunk.css"
+  },
+  {
+    "revision": "eca4012f3f90d0d01d77",
+    "url": "/mipruebaon/static/js/main.eca4012f.chunk.js"
+  },
+  {
+    "revision": "e5c3fa12dd8fdfe72cfb",
+    "url": "/mipruebaon/static/js/1.e5c3fa12.chunk.js"
+  },
+  {
+    "revision": "5e651f2fcb51d1f685c8",
+    "url": "/mipruebaon/static/js/runtime~main.5e651f2f.js"
+  },
+  {
+    "revision": "b7b4358ae8a9c93041e864e48a70e43a",
+    "url": "/mipruebaon/static/media/logoGrande.b7b4358a.png"
+  },
+  {
+    "revision": "8a2bb870608b567c2d1857fbdb94f44e",
+    "url": "/mipruebaon/static/media/cuadroGrande.8a2bb870.jpg"
+  },
+  {
+    "revision": "8e2dbad3bd5f17a67b7eda21589403a4",
+    "url": "/mipruebaon/index.html"
+  }
+];
